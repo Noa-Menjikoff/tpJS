95620ea4e06939871f626f527107506dc36ff996
noa menjikoff <noa.menjikoff@gmail.com>
2024-03-21 10:23:41 +0100

    modif css

---
+++ js/view/home.js
@@ -0,0 +1,13 @@
+import UserProvider from "../services/userprovider.js";
+import Utils from '../services/Utils.js'
+
+export default class Home {
+async render3() {
+    return `
+    <section class="information">
+    <p>home</p>
+    </section>
+    `;
+  
+    }
+}
