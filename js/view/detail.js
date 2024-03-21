99fc1750f2caa110ff5189fde6301153a6ec1aba
LANDON--NOMINE Cyril <o22202952@info12-18.iut45.univ-orleans.fr>
2024-03-21 10:09:05 +0100

    feature: ajout des details

--- js/view/detail.js
+++ js/view/detail.js
@@ -9,6 +9,23 @@ async render3() {
     <section class="information">
     <img src="${post.icon}" ">
     <p> ${post.name} </p>
+    <p> ${post.title}</p>
+    <p> ${post.description}</p>
+    <ul>
+        ${post.tags.map( genre =>
+            `<li> ${genre} </li>`
+            ).join('\n ')
+        }
+    </ul>
+    <p> Statistiques : </p>
+    <p> Attaque de départ : ${post.stats.attackdamage}</p>
+    <p> Point de vie de départ : ${post.stats.hp}</p>
+    <p> Croissance de point de vie par niveau : ${post.stats.hpperlevel}</p>
+    <p> Vitesse de mouvement : ${post.stats.movespeed}</p>
+    <p> Armure de départ : ${post.stats.armor}</p>
+    <p> Résitance magique de départ : ${post.stats.spellblock}</p>
+    <p> Vitesse d'attaque : ${post.stats.attackspeed}</p>
+    <p> Portée de l'attaque : ${post.stats.attackrange}</p>
     </section>
     <p><a href = '/'>Back to home</a></p>`;
   
